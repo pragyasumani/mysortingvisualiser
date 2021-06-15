53895f6f932113c421a7884e8a423a6d7176e776
pragya <pragya.sumani28@gmail.com>
2021-06-15 22:40:11 +0530

    updating correct name in server.js & restucturing

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/lets-visualize-sorting'));
+app.use(express.static('./dist/sorting-visualizer'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/lets-visualize-sorting/'}),
+    res.sendFile('index.html', {root: 'dist/sorting-visualizer/'}),
 );
 
 // Start the app by listening on the default Heroku port
